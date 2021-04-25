b0f6847f7075cb272b4bc3a4b7dbe1258d52a06e
AttoCat <wazasiyo2525a@gmail.com>
2021-04-25 15:20:49 +0900

    :hammer: Add some classes

--- jmapy/areas.py
+++ jmapy/areas.py
@@ -9,7 +9,7 @@ class Area:
 
 
 @dataclass
-class WeathersArea:
+class Weathers:
     area: Area
     weather_codes: List[str]
     weathers: List[str]
@@ -18,12 +18,55 @@ class WeathersArea:
 
 
 @dataclass
-class TempsArea:
+class Temps:
     area: Area
     temps: List[str]
 
 
 @dataclass
-class PopsArea:
+class Pops:
     area: Area
     pops: List[str]
+
+
+@dataclass
+class WeeklyPops:
+    area: Area
+    weather_codes: List[str]
+    pops: List[str]
+    reliabilities: List[str]
+
+
+@dataclass
+class WeeklyTemps:
+    area: Area
+    temps_min: List[str]
+    temps_min_upper: List[str]
+    temps_min_lower: List[str]
+    temps_max: List[str]
+    temps_max_upper: List[str]
+    temps_max_lower: List[str]
+
+
+@dataclass
+class TempAverage:
+    area: Area
+    min: str
+    max: str
+
+
+@dataclass
+class WeeklyTempAverage:
+    areas: List[TempAverage]
+
+
+@dataclass
+class PrecipAverage:
+    area: Area
+    min: str
+    max: str
+
+
+@dataclass
+class WeeklyPrecipAverage:
+    areas: List[PrecipAverage]
