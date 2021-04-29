2040706f11449aeabc54bcc89a4f2ac29572c0a4
AttoCat <wazasiyo2525a@gmail.com>
2021-04-29 14:36:09 +0900

    :hammer: Update _jma_get

--- jmapy/request.py
+++ jmapy/request.py
@@ -8,11 +8,13 @@ from .errors import InternalServerError, NotFound
 BASE = "https://www.jma.go.jp/bosai"
 
 
-def _jma_get(url: str, **kwargs):
+def _jma_get(url: str, raw: bool = False, **kwargs):
     response = get(f"{BASE}{url}", **kwargs)
     if response.status_code == 404:
         raise NotFound
     elif response.status_code == 500:
         raise InternalServerError
-    encoded_text = decamelize(response.text.replace("\u3000", " "))
-    return loads(encoded_text)
+    if raw:
+        return response.json()
+    encoded_text = response.text.replace("\u3000", " ")
+    return decamelize(loads(encoded_text))
