653f29a9c2bd50455583226b06c8d3093dde9541
AttoCat <attocat3141@gmail.com>
2021-11-22 23:14:51 +0900

    :sparkles: Add get_weather

--- jmapy/forecast.py
+++ jmapy/forecast.py
@@ -2,12 +2,12 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from datetime import datetime
-from typing import List, Union
+from typing import List, Tuple
 
 from dacite import Config, from_dict
 from humps import decamelize
 
-from .areas import Pops, Temps, Weathers
+from .parse_models import Area, ParsedPops, ParsedTemps, ParsedWeathers
 from .request import _jma_get
 
 
@@ -18,17 +18,49 @@ def get_forecast(area_code: int | str, raw: bool = False):
         f"/forecast/data/forecast/{area_code}.json")[0]
     if raw:
         return forecast
-    return from_dict(Forecast, decamelize(forecast), Config({datetime: datetime.fromisoformat}))
+    return from_dict(Forecast, decamelize(forecast), Config({datetime: datetime.fromisoformat}, cast=[tuple]))
+
+# search_from_areaをつくる
 
 
 @dataclass
 class Forecast:
     publishing_office: str
     report_datetime: datetime
-    time_series: List[TimeSeries]
+    time_series: Tuple[WeathersTimeSeries, PopsTimeSeries, TempsTimeSeries]
+
+    def get_weathers(self, area: str):
+        if not isinstance(area, str):
+            raise TypeError(
+                f"area argument must be bool, not {type(area).__name__}")
+        for item in self.time_series[0].areas:
+            if area not in (item.area.name, item.area.code):
+                continue
+            return Weathers(item.area, item.weathers, self.time_series[0].time_defines)
+        else:
+            return None
 
 
 @dataclass
-class TimeSeries:
+class Weathers:
+    area: Area
+    weathers: List[str]
     time_defines: List[datetime]
-    areas: List[Union[Weathers, Temps, Pops]]
+
+
+@dataclass
+class WeathersTimeSeries:
+    time_defines: List[datetime]
+    areas: List[ParsedWeathers]
+
+
+@dataclass
+class PopsTimeSeries:
+    time_defines: List[datetime]
+    areas: List[ParsedPops]
+
+
+@dataclass
+class TempsTimeSeries:
+    time_defines: List[datetime]
+    areas: List[ParsedTemps]
